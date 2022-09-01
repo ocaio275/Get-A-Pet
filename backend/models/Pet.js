20af5a387207dec5c5fb0449959293285a7010bc
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-01 20:24:40 -0300

    Criação dos Models: User e Pet

---
+++ backend/models/Pet.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const Pet = mongoose.model(
+    'Pet',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        age:{
+            type: Number,
+            required: true
+        },
+        weight:{
+            type: Number,
+            required: true
+        },
+        color:{
+            type: String
+        },
+        images:{
+            type: Array,
+            required: true
+        },
+        available:{
+            type: Boolean,
+            required: true
+        },
+        user: Object,
+        adopter: Object
+    },
+        {
+            timestamps: true
+        }
+    )
+)
+
+module.exports = Pet
---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const User = mongoose.model(
+    'User',
+    new Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String
+        },
+        phone: {
+            type: String,
+            required: true
+        }
+    },
+        {
+            timestamps: true //Adiciona coluna de Created e update
+        }
+    )
+)
+
+
+module.exports = User
