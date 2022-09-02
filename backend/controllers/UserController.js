0ca5f2b3c14107342bd2a96c4e8cc4e5a13e93f4
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-01 21:18:25 -0300

    Criando e retornando token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const createUserToken = require('../helpers/create-user-token')
 module.exports = class UserController {
     static async register(req, res) {
         const { name, email, phone, password, confirmPassword } = req.body
@@ -52,7 +53,7 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save()
-            res.status(201).json({message: 'Usuário criado com sucesso'})
+            await createUserToken(newUser, req, res)
         } catch (error) {
             res.status(500).json({message: error})
         }
---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const createUserToken = async (user, req, res) => {
+    //criando um Token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, "nossascret" //Criar dps uma secret em variavel de ambiente
+    )
+
+    //retornando um token
+    res.status(200).json({
+        message: 'Você está autenticado',
+        token: token,
+        userId: user._id
+    })
+}
+
+module.exports = createUserToken
