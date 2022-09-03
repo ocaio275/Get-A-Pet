da9777469450c5ba0870110c9878f6265d516f27
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-03 15:47:45 -0300

    Verificação do token do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -122,4 +122,9 @@ module.exports = class UserController {
         }
         res.status(200).json({user})
     }
+
+    static async editUser(req, res){
+        res.status(200).json({message: "Deu certo o Update"})
+        return
+    }
 }
---
+++ backend/helpers/verify-token.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const getToken = require('./get-token')
+
+//Middleware para validação de token
+const checkToken = (req, res, next) => {
+
+
+    if (!req.headers.authorization) {
+        return res.status(401).json(({ message: "Não autorizado" }))
+    }
+    const token = getToken(req)
+    if (!token) {
+        return res.status(401).json(({ message: "Não autorizado" }))
+    }
+
+    try {
+        const verified = jwt.verify(token, 'nossascret')
+        req.user = verified
+        next()
+    } catch (error) {
+       return res.status(400).json({message: "Token inválido"})
+        
+    }
+}
+
+module.exports = checkToken
