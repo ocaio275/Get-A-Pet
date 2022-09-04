559de509ee14a1940cdcbab51f7003f3cd1aebf8
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-04 16:41:32 -0300

    criando o component do Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 /* Components */
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
+import Container from './components/layouts/Container'
 
 /*Páginas */
 import Login from './components/pages/Auth/Login'
@@ -13,13 +14,15 @@ import Home from './components/pages/Home'
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path='/login' element={ <Login />} />
-        <Route path='/register' element={ <Register/> } />
-        <Route path='/' element={ <Home/> } />
-      </Routes>
-      <Footer/>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Container>
+      <Footer />
     </BrowserRouter>
   );
 }
