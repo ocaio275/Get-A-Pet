9992b3f030a7e83780383b46700550d758491046
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-04 11:31:13 -0300

    Função para agendamento de visitas

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -212,6 +212,56 @@ module.exports = class PetController {
         }
         await Pet.findByIdAndUpdate(id, updateData)
 
-        res.status(200).json({message: "Pet atualizado com sucesso"})
+        res.status(200).json({ message: "Pet atualizado com sucesso" })
+    }
+
+    static async schedule(req, res) {
+        const id = req.params.id
+
+        //verificar se o ObjectId é valido
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: "Id inválido" })
+            return
+        }
+
+        //Verificar se o Pet existe no banco
+        const pet = await Pet.findOne({ _id: id })
+        if (!pet) {
+            res.status(404).json({ message: "Pet não encontrado" })
+            return
+        }
+
+        //Verificar se o usuário é dono do Pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if (pet.user._id.equals(user._id)) {
+            res.status(422).json({ message: "Você não pode agendar um visita com o seu próprio Pet" })
+            return
+        }
+
+
+        //verificar se o usuário já agendou uma visita
+        if (pet.adopter) {
+            if (pet.adopter._id.equals(user._id)) {
+                res.status(422).json({ message: "Você já agendou uma visita para esse Pet" })
+                return
+            }
+        }
+
+        //adicionar o usuário ao pet
+        pet.adopter = {
+            _id: user._id,
+            name: user.name,
+            image: user.image
+        }
+
+        await Pet.findByIdAndUpdate(id, pet)
+
+        res.status(200).json({ message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone: ${pet.user.phone}` })
+    }
+
+    static async concludeAdoption(req, res){
+        
     }
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -12,5 +12,6 @@ router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifyToken ,PetController.removePetById)
 router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet)
-
+router.patch('/schedule/:id', verifyToken, PetController.schedule)
+router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption)
 module.exports = router
