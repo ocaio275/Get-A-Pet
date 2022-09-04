0c786d8d83a2fb96c1016564ca066345210d8c1c
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-04 19:31:10 -0300

    Criação de context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,19 +10,24 @@ import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 
+/**Context */
+import { UserProvider } from './context/UserContext'
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
     </BrowserRouter>
   );
 }
