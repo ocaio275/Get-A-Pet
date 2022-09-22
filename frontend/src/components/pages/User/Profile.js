5d7cc46f83b61d541f3771a148e5b90b68b5227c
Caio Oliveira <30910167+ocaio275@users.noreply.github.com>
2022-09-21 21:25:32 -0300

    Atualização de imagem do usuário

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -10,8 +10,9 @@ import Input from '../../form/Input'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 function Profile() {
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState('')
     const [token] = useState(localStorage.getItem('token') || '')
-    const {setFlashMessage} = useFlashMessage()
+    const { setFlashMessage } = useFlashMessage()
 
     useEffect(() => {
         api.get('/users/checkuser', {
@@ -28,6 +29,7 @@ function Profile() {
      * @param e - the event object
      */
     function onFileChange(e) {
+        setPreview(e.target.files[0])
         setUser({ ...user, [e.target.name]: e.target.files[0] })
     }
     /**
@@ -38,36 +40,45 @@ function Profile() {
         setUser({ ...user, [e.target.name]: e.target.value })
     }
 
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault()
-       
+
         let msgType = 'success'
 
         const formData = new FormData()
 
-        await Object.keys(user).forEach((key)=> 
+        await Object.keys(user).forEach((key) =>
             formData.append(key, user[key])
         )
-        const data = await api.patch(`/users/edit/${user._id}`, formData,{
-            headers:{
+        const data = await api.patch(`/users/edit/${user._id}`, formData, {
+            headers: {
                 Authorization: `Bearer ${JSON.parse(token)}`,
                 'Content-Type': 'multipart/form-data'
             }
         })
-        .then((response)=>{
-            return response.data
-        })
-        .catch((err) => {
-            msgType = 'error'
-            return err.response.data
-        })
+            .then((response) => {
+                return response.data
+            })
+            .catch((err) => {
+                msgType = 'error'
+                return err.response.data
+            })
         setFlashMessage(data.message, msgType)
     }
     return (
         <section>
             <div className={styles.profile_headers}>
                 <h1>Perfil</h1>
-                <p>Preview Image</p>
+                {(user.image || preview) && (
+                    <img
+                        src={
+                            preview
+                                ? URL.createObjectURL(preview)
+                                : `${process.env.REACT_APP_API}/images/users/${user.image}`
+                        }
+                        alt={user.name}
+                    />
+                )}
             </div>
 
             <form onSubmit={handleSubmit} className={formStyles.form_control}>
