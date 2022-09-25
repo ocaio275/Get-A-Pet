60ad5a851ace62619140440c4a07029c84c9614c
Caio Oliveira <caio.oliveira@vixting.com.br>
2022-09-25 20:51:41 -0300

    termindo formulário de pet

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -1,18 +1,52 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import formStyles from './Form.module.css'
-import Input from './Input' 
+import Input from './Input'
 import Select from './Select'
-function PetForm({handleSubmit ,petData, btnText}){
+function PetForm({ handleSubmit, petData, btnText }) {
     const [pet, setPet] = useState(petData || {})
     const [preview, setPreview] = useState([])
-    const colors = ["Preto", "Cinza", "Caramelo", "Branco", "Mesclado" ]
+    const colors = ["Preto", "Cinza", "Caramelo", "Branco", "Mesclado"]
 
-    function onFileChange(e){}
-    function handleChange(e){}
-    function handleColor(e){}
-    return(
-        <form className={formStyles.form_control}>
-            <Input 
+    function onFileChange(e) {
+        /* A spread operator. It is used to copy the properties of one object into another. */
+        setPreview(Array.from(e.target.files))
+        setPet({ ...pet, images: [...e.target.files] })
+    }
+    function handleChange(e) {
+        /* A spread operator. It is used to copy the properties of one object into another. */
+        setPet({ ...pet, [e.target.name]: e.target.value })
+    }
+    function handleColor(e) {
+        setPet({ ...pet, color: e.target.options[e.target.selectedIndex].text })
+    }
+
+    function submit(e) {
+        e.preventDefault()
+        console.log(pet)
+        // handleSubmit(pet)
+    }
+    return (
+        <form onSubmit={submit} className={formStyles.form_control}>
+            <div className={formStyles.preview_pet_images}>
+                {preview.length > 0
+                    ? preview.map((image, index) => (
+                        <img
+                            src={URL.createObjectURL(image)}
+                            alt={pet.name}
+                            key={`${pet.name}+${index}`}
+                        />
+                    )) :
+                    pet.images &&
+                    pet.images.map((image, index) => (
+                       <img
+                        src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                        alt={pet.name}
+                        key={`${pet.name}+${index}`}
+                       />
+                    ))
+                }
+            </div>
+            <Input
                 text="Imagens do Pet"
                 type="file"
                 name="images"
@@ -50,7 +84,7 @@ function PetForm({handleSubmit ,petData, btnText}){
                 handleOnChange={handleColor}
                 value={pet.color || ''}
             />
-            <input type="submit" value={btnText}/>
+            <input type="submit" value={btnText} />
         </form>
     )
 }
